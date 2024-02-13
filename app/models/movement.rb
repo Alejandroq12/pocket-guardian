1989cb0cd3a5807d39b1a05df281f750bfbf107c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 17:06:34 -0600

    Fix: erase unnecesary user_id un the User model and do not use length as this is used for strings.

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,7 +1,6 @@
 class Movement < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 65 }
-  validates :amount, presence: true, numericality: { greater_than: 0 }, length: { maximum: 10 }
-  validates :user_id, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :group_id, presence: true
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
