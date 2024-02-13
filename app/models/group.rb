346eea47aa404401fed02e8709a39674c91477cb
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 14:42:04 -0600

    Feat: add validations to the group model.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 65 }
+  validates :icon, presence: true
   belongs_to :user
   has_many :movements, dependent: :destroy
 end
