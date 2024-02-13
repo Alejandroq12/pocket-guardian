86c0553b7fc49934ec1c12da1f6a0537b8885b1c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 14:42:22 -0600

    Feat: add validations to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates name, presence: true, length: { minimun: 3, maximum: 65 }
+
   has_many :groups, dependent: :destroy
   has_many :movements, foreign_key: 'author_id', dependent: :destroy
 end
