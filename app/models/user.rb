585e32685f1096804eca7a745ea201997cd67261
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 17:08:36 -0600

    Fix: typo in the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { minimun: 3, maximum: 65 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 65 }
 
   has_many :groups, dependent: :destroy
   has_many :movements, foreign_key: 'author_id', dependent: :destroy
