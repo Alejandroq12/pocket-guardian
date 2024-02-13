6e3d645f99e354338962c1fe6ad94cedcbbd4076
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 17:12:11 -0600

    Fix: add column numeric capacity.

--- db/migrate/20240213023152_create_movements.rb
+++ db/migrate/20240213023152_create_movements.rb
@@ -2,7 +2,7 @@ class CreateMovements < ActiveRecord::Migration[7.1]
   def change
     create_table :movements do |t|
       t.string :name
-      t.decimal :amount
+      t.decimal :amount, precision: 10, scale: 2
       t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :group, null: false, foreign_key: true
 
