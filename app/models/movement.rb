5c07a9cd3e9835989f980dda9d9201b3be68474b
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-13 14:42:41 -0600

    Feat: add validations to the movement model.

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,9 @@
 class Movement < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 65 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }, length: { maximum: 10 }
+  validates :user_id, presence: true
+  validates :group_id, presence: true
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :group
 end
