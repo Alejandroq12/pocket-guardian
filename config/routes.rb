cba3806a3e62b33d3eddbde29708771ae94134fd
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-14 10:46:20 -0600

    Fix: routes to correctly show the splash_page.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  authenticated :user do
+    root 'home#index', as: :authenticated_root
+  end
+
+  root "splash_page#index"
 end
