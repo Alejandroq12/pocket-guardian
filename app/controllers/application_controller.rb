8f970694519d4fc20f8bdef4131a0c5985c62d49
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-14 10:49:36 -0600

    Feat: add after sign ig and sign up methods to redirect users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(_resource)
+    authenticated_root_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    authenticated_root_path
+  end
 end
