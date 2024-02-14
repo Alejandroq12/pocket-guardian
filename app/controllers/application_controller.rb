ed693511c9ae4b4ad7e6c195d50ef649dbea837d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-14 14:58:34 -0600

    Feat: enhance security, authenticate user before any action and allow custom fields for sign ups and updates.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+  end
 
   def after_sign_in_path_for(_resource)
     authenticated_root_path
