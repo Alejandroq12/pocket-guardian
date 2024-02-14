37f14e98af680c42b14abff48e09e0bb0ffc7f06
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-14 13:02:19 -0600

    Feat: Add devise modules to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true, length: { minimum: 3, maximum: 65 }
 
   has_many :groups, dependent: :destroy
