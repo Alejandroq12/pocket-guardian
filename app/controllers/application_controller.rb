5ca13504809f73682ac1a14bc4e6bbc44718bcf2
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-16 12:51:53 -0600

    Fix: content type for JSON and JS responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
+      format.json { head :forbidden, content_type: 'application/json' }
       format.html { redirect_to main_app.root_url, alert: exception.message }
-      format.js { head :forbidden, content_type: 'text/html' }
+      format.js { head :forbidden, content_type: 'application/javascript' }
     end
   end
 
