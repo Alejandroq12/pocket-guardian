66a19dcc8552ba093b5a7b585e379d1e458fd48c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-16 12:48:42 -0600

    Refactor: simplify authorization.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,7 @@ class Ability
 
     return unless user.persisted?
 
-    can :read, Movement, user_id: user.id
     can :manage, Movement, user_id: user.id
-    can :read, Group, user_id: user.id
     can :manage, Group, user_id: user.id
   end
 end
