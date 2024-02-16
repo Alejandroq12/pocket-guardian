196c2b1d1798f7e1b8964ecc7549754b81299f98
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-16 12:28:45 -0600

    Fix: linter errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-rescue_from CanCan::AccessDenied do |exception|
-  respond_to do |format|
-    format.json { head :forbidden, content_type: 'text/html' }
-    format.html { redirect_to main_app.root_url, alert: exception.message }
-    format.js { head :forbidden, content_type: 'text/html' }
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.js { head :forbidden, content_type: 'text/html' }
+    end
   end
-end
 
   protected
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.persisted?
-      can :read, Movement, user_id: user.id
-      can :manage, Movement, user_id: user.id
-      can :read, Group, user_id: user.id
-      can :manage, Group, user_id: user.id
-    end
+    return unless user.persisted?
+
+    can :read, Movement, user_id: user.id
+    can :manage, Movement, user_id: user.id
+    can :read, Group, user_id: user.id
+    can :manage, Group, user_id: user.id
   end
 end
