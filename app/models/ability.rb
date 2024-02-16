bc5881659443d56170330e9e6e175fc65d318af9
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-16 12:23:30 -0600

    Feat: add user authorization rules.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,14 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    if user.persisted?
+      can :read, Movement, user_id: user.id
+      can :manage, Movement, user_id: user.id
+      can :read, Group, user_id: user.id
+      can :manage, Group, user_id: user.id
+    end
+  end
 end
