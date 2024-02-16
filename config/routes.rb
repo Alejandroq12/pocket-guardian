4deb42aaf6d802c6449585b4873fa73d1ac0cfc5
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-16 15:32:48 -0600

    Feat: set up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,22 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
-  get 'movements/index'
-  get 'movements/show'
-  get 'movements/new'
-  get 'movements/create'
-  get 'movements/edit'
-  get 'movements/update'
-  get 'movements/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/update'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get 'users/edit/:id', to: 'users#edit', as: :edit_user
 
-  # Defines the root path route ("/")
+  resources :users, only: [] do
+    resources :groups, only: [:new, :show, :create] do
+      resources :movements, only: [:new, :show, :create]
+    end
+  end
+
+  # resources :groups, only: [:edit, :update, :destroy]
+  # recources :movements, only: [:edit, :update, :destroy]
+
   authenticated :user do
-    root 'home#index', as: :authenticated_root
+    root 'groups#index', as: :authenticated_root
   end
 
   root "splash_page#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
