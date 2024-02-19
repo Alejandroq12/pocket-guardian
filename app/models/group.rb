d8247b9c2d13c6103688861ba896a3d4c9de24f5
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-19 16:58:35 -0600

    Feat: Add Group model validations and method to get the icon file names.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,12 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 65 }
-  validates :icon, presence: true
+  # validates :icon, presence: true
+  validates :icon, presence: true, inclusion: { in: proc { Group.icon_choices } }
+
   belongs_to :user
   has_many :movements, dependent: :destroy
+
+  def self.icon_choices
+    Dir.glob('app/assets/images/group_icons/*').map { |file| File.basename(file) }
+  end
 end
