1d785f0d53ceaad989e1e9f0cf0148a5329e31b1
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-20 12:34:55 -0600

    Chore: erase comment.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 65 }
-  # validates :icon, presence: true
   validates :icon, presence: true, inclusion: { in: proc { Group.icon_choices } }
 
   belongs_to :user
