c91547190ca80c29d2d89f43b1677604198a3e37
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-26 16:13:54 -0600

    Feat: Add validation for profile_image and a method to get the images.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true, length: { minimum: 3, maximum: 65 }
+  validates :profile_image, presence: true, allow_blank: false
 
   has_many :groups, dependent: :destroy
   has_many :movements, foreign_key: 'author_id', dependent: :destroy
+
+  def self.profile_images
+    Dir.glob('app/assets/images/profile_images/*').map { |file| File.basename(file) }
+  rescue StandardError => e
+    Rails.logger.error "Failed to load icons: #{e.message}"
+  end
 end
