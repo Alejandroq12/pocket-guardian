8127d285ab497ecf6ca8c421f56be58748ce6ba4
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-27 13:59:46 -0600

    Feat: Allow :profile_image property when sign in up or editing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile_image])
   end
 
   def after_sign_in_path_for(_resource)
