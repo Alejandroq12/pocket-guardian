e5d7a329ae6cb70cf519ec2f6aea8565c92a96db
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-02 12:57:56 -0600

    Refactor: Handle scenarios in which the new movement form will create a movement having or not the parameter :group_id.

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,17 +4,34 @@ class MovementsController < ApplicationController
   def show; end
 
   def new
-    @group = current_user.groups.find(params[:group_id])
-    @movement = @group.movements.build
+    if params[:group_id]
+      @group = current_user.groups.find(params[:group_id])
+      @movement = @group.movements.build
+    else
+      @movement = Movement.new
+      @groups = current_user.groups
+    end
   end
 
   def create
-    @group = current_user.groups.find(params[:group_id])
-    @movement = @group.movements.build(movement_params)
-    @movement.author_id = current_user.id
-    if @movement.save
-      redirect_to user_group_path(current_user, @group), notice: 'Movement created sucessfully'
+    if params[:group_id]
+      # Assign the group from the parameter if available
+      @group = current_user.groups.find(params[:group_id])
+      @movement = @group.movements.build(movement_params)
     else
+      # This is for the scenario where a group is selected from a dropdown
+      @movement = current_user.movements.build(movement_params)
+    end
+
+    @movement.author_id = current_user.id
+
+    if @movement.save
+      # Use the associated group for redirection, if present
+      redirect_to @group ? user_group_path(current_user, @group) : authenticated_root_path,
+                  notice: 'Movement created successfully'
+    else
+      # Reload groups for the form in case of failure and maintain group context if present
+      @groups = current_user.groups
       render :new, status: :unprocessable_entity
     end
   end
@@ -33,6 +50,6 @@ class MovementsController < ApplicationController
   private
 
   def movement_params
-    params.require(:movement).permit(:name, :amount)
+    params.require(:movement).permit(:name, :amount, :group_id)
   end
 end
